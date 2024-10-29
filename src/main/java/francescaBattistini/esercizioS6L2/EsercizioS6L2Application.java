2b002bac3a3c117a78612f6ece7667b4f446b089
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-10-29 15:52:32 +0100

    Initial commit

---
+++ src/main/java/francescaBattistini/esercizioS6L2/EsercizioS6L2Application.java
@@ -0,0 +1,13 @@
+package francescaBattistini.esercizioS6L2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioS6L2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioS6L2Application.class, args);
+	}
+
+}
---
+++ src/test/java/francescaBattistini/esercizioS6L2/EsercizioS6L2ApplicationTests.java
@@ -0,0 +1,13 @@
+package francescaBattistini.esercizioS6L2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioS6L2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
